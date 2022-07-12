c2dd172329c995ad6545395dac3d13d1460417b1
mayronH <mayron.hen@gmail.com>
2022-07-12 11:28:42 -0300

    toast animation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,9 +11,60 @@ const triggerToast = () => {
 </script>
 <template>
   <div class="content">
-    <Toast v-if="showToast" />
+    <transition name="toast">
+      <Toast v-if="showToast" />
+    </transition>
     <Todo @bad-value="triggerToast" />
   </div>
 </template>
 
-<style></style>
+<style>
+/* .toast-enter-from {
+  opacity: 0;
+  transform: translateY(-100px);
+} */
+/* .toast-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+} */
+.toast-enter-active {
+  /* transition: opacity 0.8s ease, transform 0.4s ease; */
+  animation: wobble 0.6s ease;
+}
+/* CUSTOM CSS Animation */
+@keyframes wobble {
+  0% {
+    opacity: 0;
+    transform: translateY(-100px);
+  }
+  50% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+  60% {
+    transform: translateX(8px);
+  }
+  70% {
+    transform: translateX(-8px);
+  }
+  80% {
+    transform: translateX(4px);
+  }
+  90% {
+    transform: translateX(-4px);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+
+/* .toast-leave-from {
+  opacity: 1;
+} */
+.toast-leave-to {
+  opacity: 0;
+}
+.toast-leave-active {
+  transition: opacity 0.3s ease;
+}
+</style>
