93363c5a894d475a139796df7b2333ec31cd3f33
mayronH <mayron.hen@gmail.com>
2022-07-12 13:54:19 -0300

    javascript hooks animation

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="content about">
-    <h1>About</h1>
+    <!-- Javascript hooks -->
+    <transition
+      name="fade"
+      appear
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+    >
+      <h1 v-if="showTitle">About</h1>
+    </transition>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
       officia possimus delectus inventore quod quisquam culpa voluptas iusto,
@@ -22,11 +34,57 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, WatchCallback } from 'vue'
+import gsap from 'gsap'
 
-<style>
+const showTitle = ref(true)
+
+function beforeEnter(el: HTMLElement) {
+  console.log('before enter', el)
+  el.style.transform = 'translateY(-60px)'
+  el.style.opacity = '0'
+}
+function enter(el: HTMLElement, done: WatchCallback) {
+  console.log('enter', el)
+  gsap.to(el, {
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    ease: 'bounce.out',
+    // without onComplete: done, Vue doesn't know when the animation is done so the afterEnter is triggered immediately
+    onComplete: done,
+  })
+}
+function afterEnter(el: HTMLElement) {
+  console.log('after enter', el)
+  // el.style.color = 'green'
+  // setTimeout(() => (showTitle.value = false), 2000)
+}
+function beforeLeave(el: HTMLElement) {
+  // el.style.color = 'pink'
+  console.log('before leave', el)
+}
+function leave(el: HTMLElement) {
+  console.log('leave', el)
+}
+function afterLeave(el: HTMLElement) {
+  console.log('after leave', el)
+}
+</script>
+
+<style scoped>
 .about {
   max-width: 600px;
   margin: 20px auto;
 }
+/* 
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s ease;
+} */
 </style>
